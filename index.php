7d90db2368afab0a060eb439cbdcca39b3f7178b
mdrobin45 <developer.smrobin45@gmail.com>
2022-06-23 22:42:20 +0600

    design completed

--- index.php
+++ index.php
@@ -5,9 +5,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo List</title>
-    <link rel="stylesheet" href="./dist/output.css">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="./style/style.css">
 </head>
 <body>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div class="main">
+        <div class="container">
+            <form class="addTodoForm">
+                <input type="text" placeholder="Write todo">
+                <input type="submit" value="Add to list">
+            </form>
+            <div class="items">
+                <div class="content">
+                    <img src="./images/check.svg" alt="Check">
+                    <img src="./images/uncheck.svg" alt="uncheck">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+                    <img src="./images/delete.svg" alt="delete">
+                </div>
+            </div>
+            <div class="items">
+                <div class="content">
+                    <img src="./images/check.svg" alt="Check">
+                    <img src="./images/uncheck.svg" alt="uncheck">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+                    <img src="./images/delete.svg" alt="delete">
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
